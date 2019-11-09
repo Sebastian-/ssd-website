d2a19c3fa068bcdfbd100e32c9a43c847f8526a6
Sebastian Murgu <hmurgu@hotmail.com>
2019-11-08 17:16:41 -0800

    final integration and touchups

---
+++ dist/js/form.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    $('#myform').validate({ // initialize the plugin
+        rules: {
+            firstname: {
+                required: true,
+                minlength:2
+            },
+            lastname: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                minlength: 5
+            },
+            message: {
+                required: true,
+                minlength:5
+            }
+            
+    
+        }
+    });
+
+    $('#contact-submit').submit(function() {
+        window.location.replace("thanks.html");
+    });
+    
+});
